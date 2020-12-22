d914356e23faf5e95459a3295e18dddd8fb2c377
makiisthebes <45903624+makiisthebes@users.noreply.github.com>
2020-12-22 21:43:48 +0000

    Added Javascript File for main index html file.

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+// We are using jQuery to do this.
+// Function to remove name when user has entered a name and has clicked submit.
+// var container = $(".login_container");
+// container.fadeOut();
+
+
+myBlurFunction = function(state) {
+    /* state can be 1 or 0 */
+    var containerElement = document.getElementById('main_container');
+    var overlayEle = document.getElementById('overlay');
+
+    if (state) {
+        overlayEle.style.display = 'block';
+        containerElement.setAttribute('class', 'blur');
+    } else {
+        overlayEle.style.display = 'none';
+        containerElement.setAttribute('class', null);
+    }
+};
