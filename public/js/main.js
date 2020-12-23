f70560567290bcbcdbcc06142c0c737146028e2e
makiisthebes <45903624+makiisthebes@users.noreply.github.com>
2020-12-23 00:26:13 +0000

    Fixes and Structuring to Client Side

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,37 @@
+
 // We are using jQuery to do this.
 // Function to remove name when user has entered a name and has clicked submit.
 // var container = $(".login_container");
 // container.fadeOut();
 
 
-myBlurFunction = function(state) {
-    /* state can be 1 or 0 */
-    var containerElement = document.getElementById('main_container');
-    var overlayEle = document.getElementById('overlay');
+// This code is from http://jsfiddle.net/zrjw2a8m/
+// Used if neccessary to blur the screen when function is clicked.
 
-    if (state) {
-        overlayEle.style.display = 'block';
-        containerElement.setAttribute('class', 'blur');
-    } else {
-        overlayEle.style.display = 'none';
-        containerElement.setAttribute('class', null);
-    }
-};
+
+// Currently there is no Javascript, obfuscation will be used.
+/* https://www.youtube.com/watch?v=In0nB0ABaUk */
+
+var form = $("#form_id");
+var input = $("#name_input");
+var submit_btn = $("#name_submit");
+var notif_alert = $("#notif_alert");
+
+form.addEventListener("submit", (e) => {
+  let messages = [];
+  console.log("Test")
+  // Assigning a list the variable name messages.
+  // let allows this variable to exsist only when this function is running and not continuosly.
+  console.alert(input.value);
+
+  if (input.value.length < 1){
+    messages.push("You must enter a name before continuing ");
+  }
+
+  if (messages.length > 0){
+    e.preventDefault();
+    notif_alert.innerText = messages.join(', ');
+
+  }
+
+});
---
+++ public/js/notif.js
@@ -0,0 +1,3 @@
+// This is the script that will deal with notifications, send to the users webview.
+
+// https://www.youtube.com/watch?v=In0nB0ABaUk
